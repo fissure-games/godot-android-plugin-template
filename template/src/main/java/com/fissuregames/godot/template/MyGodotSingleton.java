0bf30cf76c6cf56ae4c2babe1c6d215d4967d176
Ross Rothenstine <rossrothenstine@gmail.com>
2023-04-10 20:07:00 -0700

    Make plugin a bit neater

--- template/src/main/java/com/fissuregames/godot/template/MyGodotSingleton.java
+++ template/src/main/java/com/fissuregames/godot/template/MyGodotSingleton.java
@@ -1,14 +1,34 @@
 package com.fissuregames.godot.template;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import org.godotengine.godot.Godot;
 import org.godotengine.godot.plugin.GodotPlugin;
+import org.godotengine.godot.plugin.SignalInfo;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class MyGodotSingleton extends GodotPlugin {
 
     public MyGodotSingleton(Godot godot) {
         super(godot);
+    }
+
+    public void sayHello() {
+        Log.i("MyGodotPlugin", "Hello from Java!");
+        emitSignal("my_signal", 42);
+    }
+
+    @NonNull
+    @Override
+    public Set<SignalInfo> getPluginSignals() {
+        Set<SignalInfo> signals = new HashSet<>();
+        signals.add(new SignalInfo("my_signal", Integer.class));
+        return signals;
     }
 
     @NonNull
